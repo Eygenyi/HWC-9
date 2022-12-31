298600f5553783c54569c3f49f59c9180fa71de8
Evgeniy <freemen0083@gmail.com>
2022-12-31 19:45:40 +0400

    #1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿string DecToBin(int n)
+{
+    string b = "";
+    if (n > 0)
+        b += DecToBin(n / 2) + (n % 2);
+    return b;
+
+}
+Console.WriteLine("Input number : ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Number in binary system: ");
+Console.WriteLine(DecToBin(n));

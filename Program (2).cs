041bf9022e4de2a989a93300df50c56fd025dc26
Evgeniy <freemen0083@gmail.com>
2022-12-31 19:46:57 +0400

    #2

---
+++ Program (2).cs
@@ -0,0 +1,16 @@
+﻿int SumNumb(int n, int m)
+{
+    if (n <= m)
+        return SumNumb(n + 1, m) + n;
+    else return 0;
+
+}
+Console.WriteLine("Input first number : ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second number : ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+if (SumNumb(n, m) == 0)
+    Console.WriteLine("Error");
+else
+    Console.WriteLine("Sum between two numbers is : " + SumNumb(n, m));
